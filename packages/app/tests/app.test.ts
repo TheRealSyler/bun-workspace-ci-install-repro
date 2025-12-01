7c1367fe0a10d0400726786d9cefab4d6d86adbd
Leonard Grosoli <hileo77@gmail.com>
2025-12-01 11:22:52 +0100

    Update

--- packages/app/tests/app.test.ts
+++ packages/app/tests/app.test.ts
@@ -1,15 +1,15 @@
-import { describe, it, expect, vi } from "vitest";
-import { runApp } from "../src/index";
+import { describe, expect, it, vi } from 'vitest'
+import { runApp } from '../src/index'
 
-describe("runApp", () => {
-  it("prints messages from app and lib", () => {
-    const logSpy = vi.spyOn(console, "log").mockImplementation(() => {});
+describe('runApp', () => {
+  it('prints messages from app and lib', () => {
+    const logSpy = vi.spyOn(console, 'log').mockImplementation(() => {})
 
-    runApp();
+    runApp()
 
-    expect(logSpy).toHaveBeenCalledWith("Hello from app");
-    expect(logSpy).toHaveBeenCalledWith("Hello from lib");
+    expect(logSpy).toHaveBeenCalledWith('Hello from app')
+    expect(logSpy).toHaveBeenCalledWith('Hello from lib!')
 
-    logSpy.mockRestore();
-  });
-});
+    logSpy.mockRestore()
+  })
+})
